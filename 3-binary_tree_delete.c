bebcbbe8681691d752fc74739176be786e25ac0d
dnjoe96 <donjoedbest@gmail.com>
2022-02-17 04:04:19 -0800

    3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,35 +1,24 @@
 #include "binary_trees.h"
-#include "binary.h"
 
+/**
+ * binary_tree_delete - a function that deletes an entire binary tree
+ *
+ * @tree: Pointer to the node
+ * Return: void
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
+	if (tree != NULL)
+	{
+		if ((tree->left == NULL) && (tree->right == NULL))
+		{
+			free(tree);
+		}
 
-  // do all this if tree does not point to null
-  if (tree != NULL)
-  {
-  // check if both left and right child are null
-    // if both are null..free this node.
-    if ((tree->left == NULL) && (tree->right == NULL))
-    {
-      free(tree);
-    }
+		if (tree->left != NULL)
+			binary_tree_delete(tree->left);
 
-    if (tree->left != NULL)
-      binary_tree_delete(tree->left);
-    if (tree->right != NULL)
-      binary_tree_delete(tree->right);
-  }
-
-    /* call delete_func on left child
-     call delete func on right child*/
-
-  // check if left child is null
-     // if null do nothing
-     // if not null call bianry_tree_delete again(pass the left child as argument
-
-  // check if right child is null
-    // if null do nothing
-    // if not null call binary_tree_delete(pass the right child as the argument)
-
-
+		if (tree->right != NULL)
+			binary_tree_delete(tree->right);
+	}
 }
