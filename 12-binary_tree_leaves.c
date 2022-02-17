3383525c880b95279466ced9efe38b09764ea6f9
dnjoe96 <donjoedbest@gmail.com>
2022-02-17 10:15:08 -0800

    Modify header file

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,29 +1,30 @@
 #include "binary_trees.h"
-#include "binary.h"
 
+/**
+ * binary_tree_leaves - a function that counts the leaves in a binary tree
+ *
+ * @tree: a pointer to the root node of the tree to count the nu of leaves
+ * Return: Count
+ */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-  size_t left;
-  size_t right;
-  size_t leaves;
+	size_t left;
+	size_t right;
+	size_t leaves;
 
-  if (tree != NULL)
-  {
+	if (tree != NULL)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return (1);
 
+		left = binary_tree_leaves(tree->left);
+		right = binary_tree_leaves(tree->right);
+		leaves = left + right;
 
-    if (tree->left == NULL && tree->right == NULL)
-      return 1;
-    /*else*/
-
-    left = binary_tree_leaves(tree->left);
-    right = binary_tree_leaves(tree->right);
-
-    leaves = left + right;
-    return leaves;
-  }
-  else
-  {
-    return 0;
-  }
-
+		return (leaves);
+	}
+	else
+	{
+		return (0);
+	}
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,25 +1,27 @@
 #include "binary_trees.h"
-#include "binary.h"
 
-
-
+/**
+ * binary_tree_height - a function that measures the height of a binary tree
+ *
+ * @tree: The pointer to the starting node
+ * Return: The height
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-  size_t left;
-  size_t right;
+	size_t left;
+	size_t right;
 
-  if (tree != NULL)
-  {
+	if (tree != NULL)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-      return 0;
-    left = 1 + binary_tree_height(tree->left);
-    right = 1 + binary_tree_height(tree->right);
+		left = 1 + binary_tree_height(tree->left);
+		right = 1 + binary_tree_height(tree->right);
 
-    if (left > right)
-      return left;
-    return right;
-  }
-
+		if (left > right)
+			return (left);
+		return (right);
+	}
 }
 
