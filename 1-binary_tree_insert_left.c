b6157c1a6ecade5f94a76c63d3d105124f0e2b03
dnjoe96 <donjoedbest@gmail.com>
2022-02-17 16:34:47 -0800

    Condition if parent is NULL'

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,6 +12,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *left_node;
 
+	if (parent == NULL)
+		return (NULL);
+
 	left_node = malloc(sizeof(binary_tree_t));
 
 	if (left_node == NULL)
