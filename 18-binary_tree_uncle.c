bd0458686b7d36988d4eb447edaf3b8251d34e60
dnjoe96 <donjoedbest@gmail.com>
2022-02-17 16:27:27 -0800

    Child of Parent parent

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,1 +1,23 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - a function that finds the uncle of a node
+ *
+ * @node: The pointer of the node
+ * Return: Return the child of the parent parent
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+	if (node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->parent == NULL)
+		return (NULL);
+
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
+}
+
